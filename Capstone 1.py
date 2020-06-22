458f8460e5d8446d31c8d7cf993fd182b9139267
aeyhk20 <67270957+aeyhk20@users.noreply.github.com>
2020-06-22 13:36:27 +0100

    Add files via upload

---
+++ Capstone 1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# This notebook will be mainly used for the capstone project
+
+
+# In[2]:
+
+
+import numpy as np
+import pandas as pd
+
+
+# In[3]:
+
+
+print('Hello Capstone Project!')
+
+
+# In[ ]:
+
+
+
+
